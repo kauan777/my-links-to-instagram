ac32d79c7d16c1c6658d0ba2bf212e8379f05a08
kauan777 <kauan7622@gmail.com>
2023-01-04 09:15:56 -0300

    feat: add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#009FB7",
+      },
+    },
   },
   plugins: [],
-}
+};
